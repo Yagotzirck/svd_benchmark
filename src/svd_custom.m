2dbef0a780c106c6171650f16940483ccadb4aaa
Alessio "Yagotzirck" Simbula <yagotzirck@gmail.com>
2025-01-27 00:21:46 +0100

    Fixed comments

--- src/svd_custom.m
+++ src/svd_custom.m
@@ -1,17 +1,19 @@
 function [U,s,V] = svd_custom(A,tau,eps_sigma)
-%SVD_CUSTOM Returns the singular values and the singular vectors of the
-%matrix A (full-rank SVD; same functionality as
+%[U,s,V] = SVD_CUSTOM(A) Returns the singular values and the singular
+%vectors of a real-valued matrix A, performing a full-rank SVD.
+%The functionality is the same as
 %[U,s,V] = svd(A, 'econ', 'vector');
+%
 %   The input parameters are:
-%       A         - A symmetric tridiagonal matrix;
+%       A         - A generic real-valued matrix;
 %
 %       tau       - (Optional) A scalar threshold used by the QR algorithm
 %                   implemented in eig_tridiag.m; see
 %                   'help eig_tridiag' for details.
 %
-%       eps_sigma - Any squared singular value below this threshold
-%                   is considered zero and discarded, along with the
-%                   singular vectors associated to them.
+%       eps_sigma - (Optional) Any squared singular value below this
+%                   threshold is considered zero and discarded, along with
+%                   the singular vectors associated to them.
 %
 %   Given:
 %       m: A's rows;
@@ -52,10 +54,12 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%% Helper function(s) %%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [X,s] = calc_singular(AA, tau, eps_sigma)
-%CALC_SINGULAR calculates the singular values s and singular vectors X,
+%CALC_SINGULAR calculates
+%   - the vector of singular values s of a matrix A;
+%   - the matrix of singular vectors X of a matrix A
 %where X are:
-%   - the left singular vectors U, if AA = A * A';
-%   - the right singular vectors V, if AA = A' * A.
+%   - the left singular vectors U of A, if AA = A * A';
+%   - the right singular vectors V of A, if AA = A' * A.
 [AA,H] = tridiag(AA);
 [X,e] = eig_tridiag(AA, tau);
 X = H * X;
